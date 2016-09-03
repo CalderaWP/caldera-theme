c73b61273ee988b42c2250699ed8b08637b925b2
Josh Pollock <jpollock412@gmail.com>
2016-09-03 15:55:36 -0400

    Fix image in single view

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ get_header(); ?>
 				$content = sprintf('<p class="taxonomy-description">%s</p>', get_the_archive_description());
 			}elseif( is_singular() ){
 				global $post;
-				$content = caldera_theme_get_part( 'content', get_post_type(), get_post() );
+				$content = caldera_theme_get_part( 'content', 'single', get_post() );
 			}else{
 				if( ! is_home() && ! empty( get_the_title() ) ){
 					$title = get_the_title();
