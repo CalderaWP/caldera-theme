d4c3af0cbc1bbb92f9b8c41702f6f66bf4db38c1
Josh Pollock <Josh@JoshPress.net>
2016-10-07 15:57:10 -0400

    add class to full width header image

--- parts/header-full.php
+++ parts/header-full.php
@@ -5,9 +5,13 @@
     if( ! isset( $img_src ) ){
         $img_src = caldera_theme_add_protocol( 'd1dy2qw4671tuy.cloudfront.net/images/cf-banner.png' );
     }
+
+    if( ! filter_var( $img_src, FILTER_VALIDATE_URL ) ){
+        return;
+    }
 ?>
 <section id="header" class="logo_only"><!-- start header section -->
     <header><!-- start header -->
-        <img src="<?php echo esc_url( $img_src ); ?>" class="img-responsive" width="100%">
+        <img src="<?php echo esc_url( $img_src ); ?>" class="img-responsive header-full" width="100%">
     </header>
 </section><!-- end collapsed -->
