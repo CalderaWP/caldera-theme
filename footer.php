d95befb9a2d30edd4b7c32379845a69e5cb420cb
Josh Pollock <Josh@JoshPress.net>
2016-11-26 17:29:06 -0500

    fix account link in footer

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 						<li><a href="#"><img src="<?php echo esc_url( caldera_theme_globe_logo( 'sm' ) ); ?>" class="img-responsive" alt=".." width="43"></a></li>
 						<li><a href="http://CalderaLabs.org" class="ft-space">Caldera Labs</a></li>
 						<li><a href="<?php echo esc_url( home_url( 'contact' ) ); ?>" class="ft-space">Contact</a></li>
-						<li><a href="#"<?php echo esc_url( home_url( 'Account' ) ); ?> class="ft-space">Account</a></li>
+						<li><a href="#"<?php echo esc_url( home_url( 'checkout/purchase-history' ) ); ?> class="ft-space">Account</a></li>
 					</ul>
 				</div>
 				<div class="col-sm-6">
