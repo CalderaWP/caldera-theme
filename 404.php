445bb7d925a70266299281f0034d224d848ce14f
Josh Pollock <Josh@JoshPress.net>
2016-11-28 20:32:19 -0500

    404 fixes

--- 404.php
+++ 404.php
@@ -4,14 +4,16 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main container" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'caldera_theme' ); ?></h1>
+			<section class="error-404 not-found row" >
+				<header class="page-header col-sm-12">
+					<h1 class="page-title">
+						<?php _e( 'Oops! That page can&rsquo;t be found.', 'caldera_theme' ); ?>
+					</h1>
 				</header><!-- .page-header -->
 
-				<div class="page-content">
+				<div class="page-content col-sm-12">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'caldera_theme' ); ?></p>
 
 					<?php get_search_form(); ?>
