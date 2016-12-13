126243f19fd444d6f9551875e669dc3693e77bea
Josh Pollock <jpollock412@gmail.com>
2016-12-12 21:53:04 -0500

    Hooks for top bar contents

--- header.php
+++ header.php
@@ -11,7 +11,6 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
-
 	<?php wp_head(); ?>
 </head>
 
@@ -19,7 +18,7 @@
 <div id="wrapper">
 	<?php
 
-	if( is_front_page() || theme::get_instance()->get_settings( get_queried_object_id() )->show_menu() ) : ?>
+	if( is_home() || is_front_page() || theme::get_instance()->get_settings( get_queried_object_id() )->show_menu() ) : ?>
 		<section id="header"><!-- start header section -->
 			<header><!-- start header -->
 				<div class="container">
@@ -39,72 +38,11 @@
 						<div class="collapse navbar-collapse navbar-ex1-collapse">
 
 							<ul class="nav navbar-nav navbar-left">
-                                <li><a href="/" class="logo navbar-brand">
-                                    <span class="">
-                                    <img style="height: 40px;width: auto;" src="<?php echo esc_url( caldera_theme_assets_uri() . '/images/caldera-globe-logo-sm.png' ); ?>" class="" alt="Caldera Forms Banner with globe logo and name">
-                                    </span>
-                                    <span class="title"><span id="title-caldera" style="color:#fff;">Caldera</span> <span id="title-forms" style="color:#a3bf61">Forms</span></span>
-                                </a></li>
+                                <?php do_action( 'caldera_theme_toppar_left' ); ?>
                             </ul>
                             <ul class="nav navbar-nav navbar-right">
 
-								<li class="dropdown">
-									<a href="<?php echo esc_url( home_url() ); ?>" id="menu-item-home" class="menu-item dropdown-toggle"  role="button" aria-haspopup="true" aria-expanded="false">Caldera Forms</a>
-									<ul class="dropdown-menu">
-										<li><a href="https://WordPress.org/plugins/caldera-forms" target="_blank">Get Started Free</a></li>
-										<li><a href="<?php echo esc_url( get_permalink( 23339 ) ); ?>">Save With Bundles</a></li>
-										<li><a href="<?php echo esc_url( get_permalink( 712 ) ); ?>">Add-ons</a></li>
-										<li role="separator" class="divider"></li>
-										<li><a href="https://Caldera.Space">Form To PDF</a></li>
-									</ul>
-								</li>
-								<li class="dropdown">
-									<a href="<?php echo esc_url( home_url( 'getting-started' ) ); ?>" id="menu-item-getting-started" class="menu-item dropdown-toggle"  role="button" aria-haspopup="true" aria-expanded="false">Getting Started</a>
-									<ul class="dropdown-menu">
-										<li><a href="<?php echo esc_url( home_url( 'getting-started' ) ); ?>">Getting Started Guide</a></li>
-										<li><a href="https://www.youtube.com/playlist?list=PLgeaHmX3MoiuXOhRlDdYn7k0RcL4afLzQ">YouTube</a></li>
-									</ul>
-								</li>
-								<li class="dropdown">
-									<a href="<?php echo esc_url( get_permalink( 1011 ) ); ?>" id="menu-item-1011" class="menu-item dropdown-toggle"  role="button" aria-haspopup="true" aria-expanded="false">Support</a>
-									<ul class="dropdown-menu">
-										<?php
-											echo caldera_theme_menu_item( 1011 );
-											echo caldera_theme_menu_item( 8128 );
-										?>
-									</ul>
-								</li>
-								<li class="dropdown">
-									<a href="<?php echo esc_url( get_permalink( 712 ) ); ?>" id="menu-item-712" class="menu-item dropdown-toggle"  role="button" aria-haspopup="true" aria-expanded="false">Add-ons</a>
-									<ul class="dropdown-menu">
-										<li>
-											<a href="<?php echo esc_url( get_permalink( 712 ) .'/#/payment' ); ?>">
-												Payment Processors
-											</a>
-										</li>
-										<li>
-                                            <a href="<?php echo esc_url( get_permalink( 712 ) .'/#/email' ); ?>">
-												Email Marketing
-											</a>
-										</li>
-										<li>
-                                            <a href="<?php echo esc_url( get_permalink( 712 ) .'/#/tools' ); ?>">
-                                                Tools
-											</a>
-										</li>
-										<li>
-                                            <a href="<?php echo esc_url( get_permalink( 712 ) .'/#/free' ); ?>">
-												Free
-											</a>
-										</li>
 
-									</ul>
-								</li>
-								<li>
-									<a href="<?php echo esc_url( home_url( 'blog' ) ); ?>" title="Caldera Forms Blog">
-										Blog
-									</a>
-								</li>
 
 							</ul>
 						</div><!-- /.navbar-collapse -->
@@ -115,6 +53,15 @@
 <?php
 	endif;
 
-	
+	/**
+	if ( ! is_front_page() ) {
+		if ( is_object( theme::get_instance()->get_settings( get_queried_object_id() )->full_width_header() ) && theme::get_instance()->get_settings( get_queried_object_id() )->full_width_header() ) {
+			echo caldera_theme_fullwidth_header();
+		}
+	}else{
+		do_action( 'caldera_theme_front_page_below_menu' );
+	}
+	**/
+	/**add-ons >**/
 
 
