6017c8ad74af0aa825296035f7e731e2ef6bec3b
Josh Pollock <jpollock412@gmail.com>
2016-12-13 00:38:55 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 use \calderawp\theme\theme;
-define( 'CALDERA_THEME_VERSION', '0.1.0' );
+define( 'CALDERA_THEME_VERSION', '0.1.2' );
 
 /**
  * Set the content width based on the theme's design and stylesheet.
