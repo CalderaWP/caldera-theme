7b8823f235723536e39b78ef130971a6c723181c
Josh Pollock <Josh@JoshPress.net>
2017-01-01 20:02:13 -0500

    use minmimzed CSS

--- functions.php
+++ functions.php
@@ -124,8 +124,8 @@ function caldera_theme_scripts() {
 
     wp_enqueue_style( 'caldera_theme-bootstrap', caldera_theme_add_protocol( 'cdn.jsdelivr.net/bootstrap/3.0.2/css/bootstrap.min.css' )  );
     wp_enqueue_style( 'caldera_theme-font-awesome', caldera_theme_add_protocol( 'cdn.jsdelivr.net/fontawesome/4.3.0/css/font-awesome.min.css' ) );
-    wp_enqueue_style( 'caldera_theme-style', caldera_theme_assets_uri() . '/css/style.css', [], CALDERA_THEME_VERSION );
-    wp_enqueue_style( 'caldera_theme-animate', caldera_theme_assets_uri() . '/css/animate.css' );
+    wp_enqueue_style( 'caldera_theme-style', caldera_theme_assets_uri() . '/css/style.min.css', [], CALDERA_THEME_VERSION );
+    wp_enqueue_style( 'caldera_theme-animate', caldera_theme_assets_uri() . '/css/animate.min.css' );
 
 
     wp_enqueue_script( 'caldera_theme-bootstrap', caldera_theme_add_protocol( 'cdn.jsdelivr.net/bootstrap/3.0.2/js/bootstrap.min.js' ), [ 'jquery' ], CALDERA_THEME_VERSION, true );
