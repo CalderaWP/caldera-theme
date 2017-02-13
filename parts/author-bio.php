6fa9154d1a73768718fa013b26fb27c23dc16971
Josh Pollock <Josh@JoshPress.net>
2017-02-13 13:23:52 -0500

    make author name a link

--- parts/author-bio.php
+++ parts/author-bio.php
@@ -1,5 +1,10 @@
 <?php
 
+$author = get_the_author();
+$author_url = get_the_author_meta('url');
+if( $author_url ) {
+	$author = sprintf( '<a rel="author" class="author-link" href="%s">%s</a>', esc_url( $author_url ), esc_html( $author ) );
+}
 ?>
 
 <div class="author-info row">
@@ -13,7 +18,7 @@
 	</div><!-- .author-avatar -->
 
 	<div class="author-description col-sm-9">
-		<h3 class="author-title" ><span class="screen-reader-text"><?php _e( 'By', 'caldera_theme' ); ?></span> <?php echo get_the_author(); ?></h3>
+		<h3 class="author-title" ><span class="screen-reader-text"><?php _e( 'By', 'caldera_theme' ); ?></span> <?php echo $author; ?></h3>
 		<p class="author-bio">
 			<?php the_author_meta( 'description' ); ?>
 		</p><!-- .author-bio -->
